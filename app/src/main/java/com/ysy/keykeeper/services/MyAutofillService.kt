60e20dbaa7a84c9d5e92b2ea883d342c73e34108
YSY <shaoyuanyu@foxmail.com>
2023-05-15 15:14:30 +0800

    进一步完善自动填充功能，适配更多app

--- app/src/main/java/com/ysy/keykeeper/services/MyAutofillService.kt
+++ app/src/main/java/com/ysy/keykeeper/services/MyAutofillService.kt
@@ -1,124 +1,155 @@
 package com.ysy.keykeeper.services
 
+import android.R.id
 import android.annotation.SuppressLint
 import android.app.assist.AssistStructure
 import android.os.Build
 import android.os.CancellationSignal
 import android.service.autofill.*
 import android.util.Log
+import android.view.View.IMPORTANT_FOR_AUTOFILL_YES
 import android.view.autofill.AutofillId
 import android.view.autofill.AutofillValue
+import android.widget.Button
 import android.widget.EditText
 import android.widget.RemoteViews
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import com.ysy.keykeeper.basic_helper.MyDatabaseHelper
 
+
 @RequiresApi(Build.VERSION_CODES.O)
 class MyAutofillService : AutofillService() {
 
     // 账号/密码识别关键词
-    val accountKeyword = arrayListOf<String>("username", "Username", "account", "Account", "用户名", "账户", "账号", "号")
-    val passwdKeyword = arrayListOf<String>("password", "Password", "密码")
+    val accountKeyword = arrayListOf<String>("username", "Username", "account", "Account", "用户名", "账户", "账号", "号", "用戶名", "賬戶", "賬號", "號")
+    val passwdKeyword = arrayListOf<String>("password", "Password", "密码", "密碼")
 
     // 解析Structure后得到的AutofillId
     data class ParsedStructure(var usernameId: AutofillId?, var passwordId: AutofillId?)
     var parsedStructure = ParsedStructure(null, null)
 
     // 账号密码
-    data class UserData(var username: String, var password: String)
+    data class UserData(var username: String?, var password: String?)
+    var userDataToSave = UserData(null, null)
 
     /**
      * 自动填充账号密码
      */
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onFillRequest(request: FillRequest, cancellationSignal: CancellationSignal, callback: FillCallback) {
+
         Log.i("自动填充", "填充请求")
+
+        // 初始化
         parsedStructure.usernameId = null
         parsedStructure.passwordId = null
 
         // Get the structure from the request
-        //val context: List<FillContext> = request.fillContexts
-        //val structure: AssistStructure = context[context.size - 1].structure
-        var fillContexts: List<FillContext> = request.getFillContexts()
-        val structure: AssistStructure = fillContexts.get(fillContexts.size - 1).getStructure()
+        val context: List<FillContext> = request.fillContexts
+        val structure: AssistStructure = context[context.size - 1].structure
 
         // Traverse the structure looking for nodes to fill out.
-        //val parsedStructure: ParsedStructure = parseStructure(structure)
         parseStructure(structure)
 
         if( this.parsedStructure.usernameId == null || this.parsedStructure.passwordId == null ) {
             Log.i("自动填充", "未检测到")
             return;
+        } else {
+            Log.i("自动填充", "检测到 ${this.parsedStructure.usernameId} ${this.parsedStructure.passwordId}")
         }
 
         // 从数据库读取账号信息
-        val accountList = fetchUserData(request)
+        val accountList = fetchUserData(request.fillContexts[0].structure.activityComponent.packageName)
         if (accountList.isEmpty()) {
+            callback.onSuccess(null)
             return;
         }
 
-        // Add a dataset to the response
+        // fillResponse
         val fillResponseBuilder = FillResponse.Builder()
-            .setSaveInfo(
-                SaveInfo.Builder(
-                    SaveInfo.SAVE_DATA_TYPE_USERNAME or SaveInfo.SAVE_DATA_TYPE_PASSWORD,
-                    arrayOf(parsedStructure.usernameId, parsedStructure.passwordId)
-                ).build()
-            )
-
 
         for (accountInfo in accountList) {
+            Log.d("自动填充", "账号 :: ${accountInfo.username}")
 
-            // Build the presentation of the datasets
             val usernamePresentation = RemoteViews(packageName, android.R.layout.simple_list_item_1)
-            usernamePresentation.setTextViewText(android.R.id.text1, accountInfo.username)
+            usernamePresentation.setTextViewText(android.R.id.text1, accountInfo.username + "账号")
+
             val passwordPresentation = RemoteViews(packageName, android.R.layout.simple_list_item_1)
-            passwordPresentation.setTextViewText(android.R.id.text1, "密码")
+            passwordPresentation.setTextViewText(android.R.id.text1, "***")
 
             val dataset = Dataset.Builder()
-                .setValue(
-                    this.parsedStructure.usernameId!!,
-                    AutofillValue.forText(accountInfo.username),
-                    usernamePresentation
-                )
-                .setValue(
-                    this.parsedStructure.passwordId!!,
-                    AutofillValue.forText(accountInfo.password),
-                    passwordPresentation
-                ).build()
+                .setField( this.parsedStructure.usernameId as AutofillId, setFieldBuilder(usernamePresentation, accountInfo.username!!).build() )
+                .setField( this.parsedStructure.passwordId as AutofillId, setFieldBuilder(null, accountInfo.password!!).build() )
+                .build()
 
             fillResponseBuilder.addDataset(dataset)
         }
-
+        fillResponseBuilder.setSaveInfo(
+            SaveInfo.Builder(
+                SaveInfo.SAVE_DATA_TYPE_USERNAME or SaveInfo.SAVE_DATA_TYPE_PASSWORD,
+                arrayOf(parsedStructure.usernameId, parsedStructure.passwordId)
+            ).build()
+        )
         val fillResponse: FillResponse = fillResponseBuilder.build()
 
-        // If there are no errors, call onSuccess() and pass the response
         callback.onSuccess(fillResponse)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    fun setFieldBuilder(presentation: RemoteViews?, text: String) : Field.Builder {
+
+        // field
+        val fieldBuilder = Field.Builder()
+
+        // value
+        //if (value != null) {
+        //    fieldBuilder.setValue(value)
+        //}
+        fieldBuilder.setValue( AutofillValue.forText( text ) )
+
+        // filter
+        //if (filter != null) {
+        //    fieldBuilder.setFilter(filter)
+        //}
+
+        // presentation
+        if ( presentation != null ) {
+            val presentationsBuilder = Presentations.Builder()
+            presentationsBuilder.setMenuPresentation(presentation)
+
+            // inlinePresentation
+            //if (inlinePresentation != null) {
+            //    presentationsBuilder.setInlinePresentation(inlinePresentation)
+            //}
+
+            // dialogPresentation
+            //if (dialogPresentation != null) {
+            //    presentationsBuilder.setDialogPresentation(dialogPresentation)
+            //}
+            presentationsBuilder.setDialogPresentation(presentation)
+
+            fieldBuilder.setPresentations( presentationsBuilder.build() )
+        }
+
+        return fieldBuilder
     }
 
     /**
      * 获取账号密码
      */
     @SuppressLint("Range")
-    fun fetchUserData(request: FillRequest): ArrayList<UserData> {
+    fun fetchUserData( thePackageName : String ): ArrayList<UserData> {
         var accountList = arrayListOf<UserData>()
 
-        var cursor = MyDatabaseHelper(this).readDbByUrl(request.fillContexts[0].structure.activityComponent.packageName)
+        var cursor = MyDatabaseHelper(this).readDbByUrl(thePackageName)
+        Log.i("自动填充 读取账号信息", "包名 :: ${thePackageName}")
 
         if ( cursor.moveToFirst() ) {
             do {
                 accountList.add( UserData( cursor.getString(cursor.getColumnIndex("account")), cursor.getString(cursor.getColumnIndex("passwd")) ) )
             } while (cursor.moveToNext())
         }
-        /*
-        val username = parsedStructure.usernameId.let { id ->
-            id?.toString() ?: ""
-        }
-
-        val password = parsedStructure.passwordId.let { id ->
-            id?.toString() ?: ""
-        }
-        */
 
         return accountList
     }
@@ -126,29 +157,11 @@ class MyAutofillService : AutofillService() {
     /**
      * 解析Structure
      */
+    @RequiresApi(Build.VERSION_CODES.P)
     fun parseStructure(structure: AssistStructure) {
         Log.i("自动填充", "检测中...")
         var usernameId: AutofillId? = null
         var passwordId: AutofillId? = null
-
-        /*
-        val nodes = structure.windowNodeCount
-
-        for (i in 0 until nodes) {
-            val node = structure.getWindowNodeAt(i)
-            val viewNode = node.rootViewNode
-
-            if ( spotKeyword(viewNode, accountKeyword) ) {
-                usernameId = viewNode.autofillId
-                Log.i("自动填充", "账号输入框")
-            }
-
-            if ( spotKeyword(viewNode, passwdKeyword) ) {
-                passwordId = viewNode.autofillId
-                Log.i("自动填充", "密码输入框")
-            }
-        }
-         */
 
         val windowNodes: List<AssistStructure.WindowNode> = structure.run { (0 until windowNodeCount).map { getWindowNodeAt(it) } }
 
@@ -156,17 +169,16 @@ class MyAutofillService : AutofillService() {
             val viewNode: AssistStructure.ViewNode? = windowNode.rootViewNode
             parseNode(viewNode)
         }
-
-        //return ParsedStructure(usernameId, passwordId)
     }
 
     /**
      * 解析Node
      * 递归
      */
+    @RequiresApi(Build.VERSION_CODES.P)
     fun parseNode(viewNode: AssistStructure.ViewNode?) {
-
         if ( spotKeyword(viewNode, accountKeyword) ) {
+            Log.d("自动填充", " ${viewNode?.id.toString()} : ${viewNode?.contentDescription.toString()} :: ${viewNode?.hint.toString()},  ${viewNode?.text.toString()}")
             if ( viewNode?.autofillId != null ) {
                 this.parsedStructure.usernameId = viewNode?.autofillId
             }
@@ -187,25 +199,36 @@ class MyAutofillService : AutofillService() {
      * 关键词检测
      * 用于判断账号/密码输入框
      */
-    fun spotKeyword( viewNode: AssistStructure.ViewNode?, keywordList : ArrayList<String> ) : Boolean {
+    @RequiresApi(Build.VERSION_CODES.P)
+    fun spotKeyword(viewNode: AssistStructure.ViewNode?, keywordList : ArrayList<String> ) : Boolean {
 
-        Log.d("自动填充", "classname :: ${viewNode?.className.toString()}, ${EditText::class.java.name.toString()}")
+        // 黑名单
+        //
+        // ----
 
-        if ( viewNode?.className.equals(EditText::class.java.name) || viewNode?.className.equals(TextView::class.java.name) ) {
-            Log.d("自动填充", " ${viewNode?.id.toString()} :: ${viewNode?.hint.toString()},  ${viewNode?.text.toString()}")
+        //Log.d("自动填充 关键词识别", "${viewNode?.autofillHints}, ${viewNode?.importantForAutofill}")
 
-            for (word in keywordList) {
-                if ( viewNode?.autofillHints?.contains(word) == true ) {
-                    Log.i("自动填充", "检测到")
+        // 筛选
+        for (word in keywordList) {
+            if ( viewNode?.autofillHints?.contains(word) == true ) {
+                // 优先通过autofillHints判断
+                return true
+            } else if ( viewNode?.className.equals(EditText::class.java.name) || viewNode?.isFocusable == true ) {
+                // || viewNode?.isFocusable == true
+
+                // 黑名单
+                if ( viewNode?.className.equals(Button::class.java.name) ) {
+                    return false
+                }
+                if ( viewNode?.hint.toString().contains("忘记密码") || viewNode?.text.toString().contains("忘记密码") ) {
+                    return false
+                }
+
+                if (viewNode?.contentDescription.toString().contains(word)) {
                     return true
-                } else if ( viewNode?.id.toString().contains(word) ) {
-                    Log.i("自动填充", "检测到")
+                } else if (viewNode?.hint.toString().contains(word)) {
                     return true
-                } else if ( viewNode?.hint.toString().contains(word) ) {
-                    Log.i("自动填充", "检测到")
-                    return true
-                } else if ( viewNode?.text.toString().contains(word) ) {
-                    Log.i("自动填充", "检测到")
+                } else if (viewNode?.text.toString().contains(word)) {
                     return true
                 }
             }
@@ -214,14 +237,16 @@ class MyAutofillService : AutofillService() {
         return false
     }
 
-    fun getAccountInfo() {
 
-    }
 
     /**
      * 保存新账号密码
      */
     override fun onSaveRequest(request: SaveRequest, callback: SaveCallback) {
+
+        this.userDataToSave.username = null
+        this.userDataToSave.password = null
+
         // Get the structure from the request
         val context: List<FillContext> = request.fillContexts
         val structure: AssistStructure = context[context.size - 1].structure
@@ -233,6 +258,7 @@ class MyAutofillService : AutofillService() {
         callback.onSuccess()
     }
 
+    @RequiresApi(Build.VERSION_CODES.P)
     fun traverseStructure(structure: AssistStructure) {
         val windowNodes: List<AssistStructure.WindowNode> = structure.run { (0 until windowNodeCount).map { getWindowNodeAt(it) } }
 
@@ -242,10 +268,14 @@ class MyAutofillService : AutofillService() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.P)
     fun traverseNode(viewNode: AssistStructure.ViewNode?) {
         if (viewNode?.autofillHints?.isNotEmpty() == true) {
-            // If the client app provides autofill hints, you can obtain them using:
-            // viewNode.getAutofillHints();
+            if ( spotKeyword(viewNode, accountKeyword) ) {
+                this.userDataToSave.username = viewNode?.text.toString()
+            } else if ( spotKeyword(viewNode, passwdKeyword) ) {
+                this.userDataToSave.password = viewNode?.text.toString()
+            }
         } else {
             // Or use your own heuristics to describe the contents of a view
             // using methods such as getText() or getHint().
